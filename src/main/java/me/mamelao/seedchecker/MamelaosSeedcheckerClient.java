536fa317b29332c4a79cb9a73b357df0dfd4fce7
mamelao <mamelao@interia.pl>
2026-05-10 16:38:38 +0200

    First commit

---
+++ src/main/java/me/mamelao/seedchecker/MamelaosSeedcheckerClient.java
@@ -0,0 +1,10 @@
+package me.mamelao.seedchecker;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MamelaosSeedcheckerClient  implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/me/mamelao/seedchecker/MamelaosSeedcheckerDataGenerator.java
@@ -0,0 +1,11 @@
+package me.mamelao.seedchecker;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MamelaosSeedcheckerDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
